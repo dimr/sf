036eabea221d638ef4c168e2405976ec487adbc8
dimr <dimitris.rongotis@gmail.com>
2015-04-18 17:04:00 +0300

    added text vertically+formating

--- src/Button.java
+++ src/Button.java
@@ -3,165 +3,165 @@ import toxi.geom.Vec2D;
 
 public class Button {
 
-	private PApplet pa;
-	private Vec2D position;
-	private int h, w;
-	private String lineAbbreviation;
-	private float size;
-	private int lineID;
-	private int counter;
-	private String name;
-	private Vec2D mouseLocInitital;
-	private int buttonCounter = 0;
-	int previous = -1;
-
-	
-	public Button(PApplet pa, Vec2D position, int w, int h, int lineID,String lineAbbreviation, String name) {
-		this.pa = pa;
-		this.position = position;
-		this.w = w;
-		this.lineID = lineID;
-		
-		this.h = h;
-		this.lineAbbreviation = lineAbbreviation;
-		size = 0;
-		this.name = name;
-		pa.textFont(SanMain.font, 12);
-
-	}
-	
-	
-	public int getLineID() {
-		return lineID;
-	}
+    private PApplet pa;
+    private Vec2D position;
+    private int h, w;
+    private String lineAbbreviation;
+    private float size;
+    private int lineID;
+    private int counter;
+    private String name;
+    private Vec2D mouseLocInitital;
+    private int buttonCounter = 0;
+    int previous = -1;
 
 
-	public void setLineID(int lineID) {
-		this.lineID = lineID;
-	}
+    public Button(PApplet pa, Vec2D position, int w, int h, int lineID, String lineAbbreviation, String name) {
+        this.pa = pa;
+        this.position = position;
+        this.w = w;
+        this.lineID = lineID;
+
+        this.h = h;
+        this.lineAbbreviation = lineAbbreviation;
+        size = 0;
+        this.name = name;
+        pa.textFont(SanMain.font, 12);
+
+    }
 
 
-	public Vec2D getPosition() {
-		return position;
-	}
-
-	public void setPosition(Vec2D position) {
-		this.position = position;
-	}
-
-	public void  setPosition(float x,float y){
-		this.position.set(x, y);
-	}
-	public int getH() {
-		return h;
-	}
-
-	public void setH(int h) {
-		this.h = h;
-	}
-
-	public int getW() {
-		return w;
-	}
-
-	public void setW(int w) {
-		this.w = w;
-	}
-
-	public String getLineAbbreviation (){
-		return this.lineAbbreviation;
-	}
-
-	public void setId(String lineAbbr) {
-		this.lineAbbreviation = lineAbbr;
-	}
+    public int getLineID() {
+        return lineID;
+    }
 
 
+    public void setLineID(int lineID) {
+        this.lineID = lineID;
+    }
 
-	public int setPrevious(int lineID) {
-		return this.previous = lineID;
-	}
 
-	public int getPrevious() {
-		return this.previous;
-	}
+    public Vec2D getPosition() {
+        return position;
+    }
 
-	public String getName(){
-		return this.name;
-	}
+    public void setPosition(Vec2D position) {
+        this.position = position;
+    }
 
-	public void drawButton() {
-		pa.pushStyle();
-		int previous=-1;
-		if (inside()) {
-			pa.fill(89, 125, 132);
-			counter++;
-			//delay a bit 
-			if (counter > 20) {
-				pa.pushStyle();
-				pa.fill(100, 60);
-				size = pa.lerp(size, name.length(), .3f);
-				pa.rect(position.x + w + 10, position.y, size*10 , h, 5);
-				pa.popStyle();
+    public void setPosition(float x, float y) {
+        this.position.set(x, y);
+    }
 
-			}
-			//delay text a bit more
-			if (counter > 35) {
-				// pa.textMode(pa.SCREEN);
-				pa.pushStyle();
-				pa.fill(255);
-				
-				pa.text(name, position.x + w + 20, position.y + h / 2);// +h/4+20);
-				pa.popStyle();
-			}
-		} // IF INSIDE
-		else {
-			pa.fill(89, 125, 132, 70);
-			counter = 0;
-			size = 0;
-			// setLineID(-1);
-			// setPrevious(-1);
-			// System.out.println("COUNTER RESET");
-			// pa.cursor(pa.ARROW);
-		}
+    public int getH() {
+        return h;
+    }
 
-		// pa.rectMode(pa.CORNER);
-		pa.rect(position.x, position.y, w, h, 5);
-		pa.popStyle();
-		// pa.pushStyle();
-		// pa.fill(0);
-		pa.text(lineAbbreviation, position.x + 10, position.y + h / 2);
-		// pa.ellipse(position.x+w/2,position.y+h/2,10,10);
-		// pa.pStyle();
+    public void setH(int h) {
+        this.h = h;
+    }
 
-	}
+    public int getW() {
+        return w;
+    }
 
-	public Vec2D getMouseLocInitital() {
-		return mouseLocInitital;
-	}
+    public void setW(int w) {
+        this.w = w;
+    }
 
-	public void setMouseLocInitital(Vec2D mouseLocInitital) {
-		this.mouseLocInitital = mouseLocInitital;
-	}
+    public String getLineAbbreviation() {
+        return this.lineAbbreviation;
+    }
 
-	public boolean inside() {
-		if (pa.mouseX >= position.x && pa.mouseX <= position.x + w
-				&& pa.mouseY >= position.y && pa.mouseY <= position.y + h) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public void setId(String lineAbbr) {
+        this.lineAbbreviation = lineAbbr;
+    }
 
-	public void navigate() {
-		counter = 0;
-		// if (inside()){
-		mouseLocInitital = new Vec2D(pa.mouseX, pa.mouseY);
-		// if ()
-		// position.set(position.x,pa.lerp(position.y, position.y+3, .4f));
-		// position.set
-		// }else{
-		// }
 
-	}
+    public int setPrevious(int lineID) {
+        return this.previous = lineID;
+    }
+
+    public int getPrevious() {
+        return this.previous;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void drawButton() {
+        pa.pushStyle();
+        int previous = -1;
+        if (inside()) {
+            pa.fill(89, 125, 132);
+            counter++;
+            //delay a bit
+            if (counter > 20) {
+                pa.pushStyle();
+                pa.fill(100, 60);
+                size = pa.lerp(size, name.length(), .3f);
+                pa.rect(position.x + w + 10, position.y, size * 10, h, 5);
+                pa.popStyle();
+
+            }
+            //delay text a bit more
+            if (counter > 35) {
+                // pa.textMode(pa.SCREEN);
+                pa.pushStyle();
+                pa.fill(255);
+
+                pa.text(name, position.x + w + 20, position.y + h / 2);// +h/4+20);
+                pa.popStyle();
+            }
+        } // IF INSIDE
+        else {
+            pa.fill(89, 125, 132, 70);
+            counter = 0;
+            size = 0;
+            // setLineID(-1);
+            // setPrevious(-1);
+            // System.out.println("COUNTER RESET");
+            // pa.cursor(pa.ARROW);
+        }
+
+        // pa.rectMode(pa.CORNER);
+        pa.rect(position.x, position.y, w, h, 5);
+        pa.popStyle();
+        // pa.pushStyle();
+        // pa.fill(0);
+        pa.text(lineAbbreviation, position.x + 10, position.y + h / 2);
+        // pa.ellipse(position.x+w/2,position.y+h/2,10,10);
+        // pa.pStyle();
+
+    }
+
+    public Vec2D getMouseLocInitital() {
+        return mouseLocInitital;
+    }
+
+    public void setMouseLocInitital(Vec2D mouseLocInitital) {
+        this.mouseLocInitital = mouseLocInitital;
+    }
+
+    public boolean inside() {
+        if (pa.mouseX >= position.x && pa.mouseX <= position.x + w
+                && pa.mouseY >= position.y && pa.mouseY <= position.y + h) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void navigate() {
+        counter = 0;
+        // if (inside()){
+        mouseLocInitital = new Vec2D(pa.mouseX, pa.mouseY);
+        // if ()
+        // position.set(position.x,pa.lerp(position.y, position.y+3, .4f));
+        // position.set
+        // }else{
+        // }
+
+    }
 }
--- src/DataLoader.java
+++ src/DataLoader.java
@@ -12,116 +12,116 @@ import toxi.geom.Vec2D;
 
 public class DataLoader {
 
-	public static ArrayList<Station> loadStations(String json_file) {
-		ArrayList<Station> stations = new ArrayList<Station>();
-		JSONParser parser = new JSONParser();
-		Object in = null;
-		try {
-			in = parser.parse(new FileReader(json_file));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		JSONObject start = (JSONObject) in;
-		JSONArray features = (JSONArray) start.get("features");
-		for (int i = 0; i < features.size(); i++) {
-			JSONObject properties = (JSONObject) features.get(i);
-			JSONObject pr = (JSONObject) properties.get("properties");
-			String name = (String) pr.get("STOPNAME");
-			double lon = ((Double) pr.get("LONGITUDE")).doubleValue();
-			double lat = ((Double) pr.get("LATITUDE")).doubleValue();
-			float f_lo = (float) lon;
-			float f_lat = (float) (lat);
-			double stop_id = ((Double) pr.get("STOPID")).doubleValue();
-			stations.add(new Station(name, (float) stop_id, new Vec2D(f_lo, f_lat)));
-		}
-		// System.out.println(stations.size()+" Stations : Loaded ");
+    public static ArrayList<Station> loadStations(String json_file) {
+        ArrayList<Station> stations = new ArrayList<Station>();
+        JSONParser parser = new JSONParser();
+        Object in = null;
+        try {
+            in = parser.parse(new FileReader(json_file));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        JSONObject start = (JSONObject) in;
+        JSONArray features = (JSONArray) start.get("features");
+        for (int i = 0; i < features.size(); i++) {
+            JSONObject properties = (JSONObject) features.get(i);
+            JSONObject pr = (JSONObject) properties.get("properties");
+            String name = (String) pr.get("STOPNAME");
+            double lon = ((Double) pr.get("LONGITUDE")).doubleValue();
+            double lat = ((Double) pr.get("LATITUDE")).doubleValue();
+            float f_lo = (float) lon;
+            float f_lat = (float) (lat);
+            double stop_id = ((Double) pr.get("STOPID")).doubleValue();
+            stations.add(new Station(name, (float) stop_id, new Vec2D(f_lo, f_lat)));
+        }
+        // System.out.println(stations.size()+" Stations : Loaded ");
 
-		// for (Station s: stations)
-		// System.out.println(stations.get(0));
-		// System.out.println(s.getStopID()+" "+s.getStopName());
-		return stations;
-	}
+        // for (Station s: stations)
+        // System.out.println(stations.get(0));
+        // System.out.println(s.getStopID()+" "+s.getStopName());
+        return stations;
+    }
 
-	public static ArrayList<Route> loadRoutes(PApplet pa, String json_file) {
-		ArrayList<Route> routes = new ArrayList<Route>();
-		JSONParser parser = new JSONParser();
-		Object in = null;
-		try {
-			in = parser.parse(new FileReader(json_file));
-		} catch (Exception e) {
-		}
+    public static ArrayList<Route> loadRoutes(PApplet pa, String json_file) {
+        ArrayList<Route> routes = new ArrayList<Route>();
+        JSONParser parser = new JSONParser();
+        Object in = null;
+        try {
+            in = parser.parse(new FileReader(json_file));
+        } catch (Exception e) {
+        }
 
-		JSONObject start = (JSONObject) in;
-		JSONArray features = (JSONArray) start.get("features");
-		for (int i = 0; i < features.size(); i++) {
+        JSONObject start = (JSONObject) in;
+        JSONArray features = (JSONArray) start.get("features");
+        for (int i = 0; i < features.size(); i++) {
 
-			JSONObject pr = (JSONObject) features.get(i);
-			JSONObject properties = (JSONObject) pr.get("properties");
-			String lineAbbr = (String) properties.get("LINEABBR");
-			String final_lineAbbr = allDigits(lineAbbr) ? fix(lineAbbr) : "X";
-			String stop_id = (String) properties.get("STOPID");
-			String name = (String) properties.get("LINENAME");
-			long theID = ((Long) properties.get("LINEID")).longValue();
-			int line_id = (int) theID;
+            JSONObject pr = (JSONObject) features.get(i);
+            JSONObject properties = (JSONObject) pr.get("properties");
+            String lineAbbr = (String) properties.get("LINEABBR");
+            String final_lineAbbr = allDigits(lineAbbr) ? fix(lineAbbr) : "X";
+            String stop_id = (String) properties.get("STOPID");
+            String name = (String) properties.get("LINENAME");
+            long theID = ((Long) properties.get("LINEID")).longValue();
+            int line_id = (int) theID;
 
-			JSONObject ar = (JSONObject) pr.get("geometry");
-			JSONArray ar2 = (JSONArray) ar.get("coordinates");
-			if (!final_lineAbbr.equals("X"))
-				routes.add(new Route(pa, final_lineAbbr, name, line_id, new LineSegments(pa, ar2)));
-		}
+            JSONObject ar = (JSONObject) pr.get("geometry");
+            JSONArray ar2 = (JSONArray) ar.get("coordinates");
+            if (!final_lineAbbr.equals("X"))
+                routes.add(new Route(pa, final_lineAbbr, name, line_id, new LineSegments(pa, ar2)));
+        }
 
-		// for (Route r : routes)
-		// System.out.println(r.getLineAbbr() + " " + fix(r.getLineAbbr()));
-		return routes;
-	}
+        // for (Route r : routes)
+        // System.out.println(r.getLineAbbr() + " " + fix(r.getLineAbbr()));
+        return routes;
+    }
 
-	private static String fix(String name) {
-		char[] a = name.toCharArray();
-		StringBuilder result = new StringBuilder();
-		if (a[0] == '0' && a[1] != '0') {
-			for (int i = 1; i < a.length; i++) {
-				result.append(a[i]);
-			}
-			return result.toString();
+    private static String fix(String name) {
+        char[] a = name.toCharArray();
+        StringBuilder result = new StringBuilder();
+        if (a[0] == '0' && a[1] != '0') {
+            for (int i = 1; i < a.length; i++) {
+                result.append(a[i]);
+            }
+            return result.toString();
 
-		} else if (a[0] == '0' && a[1] == '0') {
-			for (int i = 2; i < a.length; i++) {
-				result.append(a[i]);
-			}
-			return result.toString();
-		}
-		return name;
-	}
+        } else if (a[0] == '0' && a[1] == '0') {
+            for (int i = 2; i < a.length; i++) {
+                result.append(a[i]);
+            }
+            return result.toString();
+        }
+        return name;
+    }
 
-	private static boolean allDigits(String name) {
-		boolean result = true;
-		char[] a = name.toCharArray();
-		for (int i = a.length - 1; i >= 0; i--) {
-			if (Character.isAlphabetic(a[i])) {
-				return !result;
-			}
-		}
-		return result;
-	}
+    private static boolean allDigits(String name) {
+        boolean result = true;
+        char[] a = name.toCharArray();
+        for (int i = a.length - 1; i >= 0; i--) {
+            if (Character.isAlphabetic(a[i])) {
+                return !result;
+            }
+        }
+        return result;
+    }
 
-	public static HashMap<String, ArrayList<String>> getStationsPerRoute(String filename) {
-		Map<String, ArrayList<String>> result = new HashMap<String, ArrayList<String>>();
-		JSONParser parser = new JSONParser();
-		Object in = null;
-		try {
-			in = parser.parse(new FileReader(filename));
+    public static HashMap<String, ArrayList<String>> getStationsPerRoute(String filename) {
+        Map<String, ArrayList<String>> result = new HashMap<String, ArrayList<String>>();
+        JSONParser parser = new JSONParser();
+        Object in = null;
+        try {
+            in = parser.parse(new FileReader(filename));
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		JSONObject start = (JSONObject) (in);
-		for (Object ss : start.keySet()) {
-			if (((JSONArray) start.get(ss)).size() != 0) {
-				// System.out.println(ss+" "+start.get(ss));
-				result.put((String) ss, (ArrayList) start.get(ss));
-			}
-		}
-		return (HashMap<String, ArrayList<String>>) result;
-	}
+        JSONObject start = (JSONObject) (in);
+        for (Object ss : start.keySet()) {
+            if (((JSONArray) start.get(ss)).size() != 0) {
+                // System.out.println(ss+" "+start.get(ss));
+                result.put((String) ss, (ArrayList) start.get(ss));
+            }
+        }
+        return (HashMap<String, ArrayList<String>>) result;
+    }
 }
